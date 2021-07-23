dbe6373cdc1c5a2e9ade83155d35fea04b46dad0
opatrickmachado <opatrickmachado@gmail.com>
2021-07-22 21:40:47 -0300

    VueJsPulses

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Teste Pulses</h1>
+  <p>Aplicação criada com Vue CLI por Patrick Machado</p>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
